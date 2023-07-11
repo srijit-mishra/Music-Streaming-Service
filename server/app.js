8873c9e74e270c32a2107d7fee87c1e174c177e1
Srijit <mishrasrijit@gmail.com>
2023-07-11 15:17:37 +0530

    First Commit
    This is the GitHub repository for a music streaming service that utilizes modern web technologies to deliver a seamless and enjoyable listening experience. The service is built on a back-end stack consisting of NodeJS, Express, and MongoDB, which provide a robust and scalable infrastructure for storing and serving music data. Firebase is used for user authentication and real-time updates, ensuring that the service is secure and responsive.  On the front-end, React is used to create a fast and dynamic user interface that allows users to search, browse, and play their favorite songs and playlists. The service also features social sharing capabilities, allowing users to share their music with friends and followers on popular social media platforms.  This GitHub repository contains all the code and resources needed to deploy and run the music streaming service.

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const uri =
+  "";
+
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// user authentication routes
+const userRoute = require("./routes/auth");
+app.use("/api/users/", userRoute);
+
+// Artist links
+const artistsRoute = require("./routes/artists");
+app.use("/api/artists/", artistsRoute);
+
+// Album links
+const albumRoute = require("./routes/albums");
+app.use("/api/albums/", albumRoute);
+
+// Songs links
+const songRoute = require("./routes/songs");
+app.use("/api/songs/", songRoute);
+
+// If any depreciation warning add depreciation options
+// mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true }, () => {
+//   console.log("Mongodb Connected");
+// });
+
+mongoose.connect(uri, { useNewUrlParser: true });
+
+mongoose.connection
+  .once("open", () => console.log("Connected"))
+  .on("error", (error) => {
+    console.log(`Error : ${error}`);
+  });
+
+app.listen(4000, () => console.log("lisitening to port 4000"));
